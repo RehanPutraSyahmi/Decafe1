1ae9d93f4d24eeaa5cec3985a61913bfa61d8c60
RehanPutraSyahmi <149367154+RehanPutraSyahmi@users.noreply.github.com>
2023-10-30 17:14:32 +0700

    Update connect.php

--- proses/connect.php
+++ proses/connect.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","db_decafe");
-    if(!$conn){
-        echo "Gagal koneksi";
-    }
+$conn = mysqli_connect("localhost", "u837952942_reyhan", “contoh", "u837952942_reyhan");
+if (!$conn){
+ echo "Gagal koneksi";
+ }
 ?>
